4cbe9540c8053ac6108c59a8b663df44ab33d228
Koko <hanky_h@qq.com>
2022-04-14 16:49:48 -0500

    change the animals type into emoji

--- animalChess.py
+++ animalChess.py
@@ -29,6 +29,7 @@ class AnimalChess:
         self.board = [[None for i in range(4)] for i in range(4)]
         # generate pieces
         pieces_list = []
+
         for belonging in range(2):
             for animal in range(8):
                 piece = Piece(animal, belonging)
@@ -42,10 +43,26 @@ class AnimalChess:
                 piece_idx += 1
 
     def print_board(self):
+
+        # animalList : [0, 1, 2, 3, 4, 5, 6, 7] # Elephant > Lion > Tiger > Leopard > Dog > Woof > Cat > Rat | ADD # 0 can eat 7
+        # animalsMap = {0: "Rat", 1: "Cat", 2:"Woof", 3: "Dog", 4: "Leopard", 5: "Tiger", 6: "Lion", 7: "Elephant"}
+
+        animalsMap = {
+            0: "\U0001F401",
+            1: "\U0001F408",
+            2: "\U0001F43A",
+            3: "\U0001F436",
+            4: "\U0001F406",
+            5: "\U0001F42F",
+            6: "\U0001F981",
+            7: "\U0001F418"}
+
+        belongingsMap = {0: "A", 1: "B"}
+
         for i in range(4):
             for j in range(4):
                 piece = self.board[i][j]
-                print(piece.animal, piece.belongings, end="|")
+                print(animalsMap[piece.animal], belongingsMap[piece.belongings], end="|")
             print()
 
     def decide_the_winner(self):
